6bca453f6119e87a3c2525f59a97c4f8702533cb
Akshat-Dwivedi <akshat10dwivedi@gmail.com>
2023-11-25 12:08:27 +0530

    home page done

--- Rock-Paper-Scissors/index.js
+++ Rock-Paper-Scissors/index.js
@@ -1,1 +1,10 @@
 const getById = (id) => document.getElementById(id);
+
+window.showRules = () => {
+  getById("rules").style.display = "block";
+  // getById("next_box").style.display = "flex";
+};
+
+window.closeBtn = () => {
+  getById("rules").style.display = "none";
+};
